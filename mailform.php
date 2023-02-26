b34e6df40b3f4fefca48fa7d0fd5fb9740e9af4d
J0shP4tl1ngr40 <jpat936@aucklanduni.ac.nz>
2023-02-26 13:40:26 +1300

    Create mailform.php
    - This is the PHP file to send the details of the contact form - customer name, email and message - to the website owner.
    - Still needs modification to fit into Kuya Nick's website and further details of hosting.
    - May need additional PHPMail folder file.

---
+++ mailform.php
@@ -0,0 +1,87 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+#require_once __DIR__ . 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
+
+$errors = [];
+$error_message = '';
+
+if (!empty($_POST)) {
+    #Access the data entered by customer
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    #Email of website owner
+    $to_email = 'nickkitchen@auckland.co.nz';
+
+    #Missing info check
+    if (empty($name)) {
+        $errors[] = 'No name entered';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'No email entered';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Invalid email';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'No message entered';
+    }
+
+    #Check if any errors are triggered
+    if (empty($errors)) {
+        #Create new instance of PHPMailer
+        $mail = new PHPMailer;
+
+        #Server settings
+        #Set PHPMailer to use SMTP
+        $mail->isSMTP();
+        #Set SMTP host name
+        $mail->Host = 'smtp.mailtrap.io';
+        #Enable SMTP authentication
+        $mail->SMTPAuth = true;
+        #SMTP username and password
+        $mail->Username = '';
+        $mail->Password = '';
+        #If SMTP requires TLS encryption then set it
+        $mail->SMTPSecure = 'tls';
+        #Set TCP port to connect to
+        $mail->Port = 2525;
+
+        #User email details
+        $mail->setFrom($email, $name);
+        $mail->addReplyTo($email, $name);
+        #Owner email
+        $mail->addAddress($to_email, 'Me');
+        #Set email format to HTML
+        $mail->isHTML(true);
+        #Email content
+        $mail->Subject = "New message from Nick's Kitchen";
+        $mail->Body = "Hi Nick,\n\nYou have received a new message from $name.\n\nEmail: $email\nMessage: $message";
+
+
+        echo $message;
+
+        if ($mail->send()) {
+            echo "Message has been sent successfully";
+        } else {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        }
+
+        #Return to section of website once form is submitted
+        header('location:index.php#contact');
+
+        #$mail->From = $email;
+        #$mail->FromName = $name;
+        #$mail->setFrom($email, 'Mailtrap Website'); //Rename to actual SMTP host
+    } else {
+        #Display error to user
+        $all_errors = join('<br/>', $errors);
+        $error_message = "<p style='color: red;'>{$all_errors}</p>";
+    }
+}
+
+?>
